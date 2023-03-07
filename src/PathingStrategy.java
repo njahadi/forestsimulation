a95701aeaca957b163790db61d53508390fe3b54
Nickaan Jahadi <njahadi@calpoly.edu>
2023-03-06 16:55:42 -0800

    update

---
+++ src/PathingStrategy.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.Stream;
+
+public interface PathingStrategy {
+    /*
+     * Returns a prefix of a path from the start point to a point within reach
+     * of the end point.  This path is only valid ("clear") when returned, but
+     * may be invalidated by movement of other entities.
+     *
+     * The prefix includes neither the start point nor the end point.
+     */
+    List<Point> computePath(Point start, Point end,
+                            Predicate<Point> canPassThrough,
+                            BiPredicate<Point, Point> withinReach,
+                            Function<Point, Stream<Point>> potentialNeighbors);
+
+    static final Function<Point, Stream<Point>> CARDINAL_NEIGHBORS =
+            point ->
+                    Stream.<Point>builder()
+                            .add(new Point(point.getX(), point.getY() - 1))
+                            .add(new Point(point.getX(), point.getY() + 1))
+                            .add(new Point(point.getX() - 1, point.getY()))
+                            .add(new Point(point.getX() + 1, point.getY()))
+                            .build();
+}
